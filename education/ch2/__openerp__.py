4c807793aa7266c1545d9fa9670732202ac1b7f9
odoo <kimey583@gmail.com>
2016-09-29 01:32:54 -0700

    9.23.2

--- education/ch2/__openerp__.py
+++ education/ch2/__openerp__.py
@@ -1,12 +1,10 @@
 {
     'name': 'Education 1',
-    'description':""" """,
     'author': 'alltop',
-    'depends': ['mail'],
     'application': True,
     'data': [
-        'todo_view.xml',
         'security/ir.model.access.csv',
         'security/todo_access_rules.xml',
+        'views/todo_view.xml'
     ]
 }
