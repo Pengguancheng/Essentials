cb1b87aa455235296d933be68b40e2794c6b3848
odoo <kimey583@gmail.com>
2016-10-12 19:35:05 -0700

    10.13 CH5.6

---
+++ education/ch5/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': "CH5,6",
+    'version': '1.0',
+    'depends': [
+                    'ch3'
+                ],
+    'author': "alltio",
+    'website': "http://eoffice.alltop.com.tw",
+    'category': '',
+    'description': """
+    """,
+    'data': ['views/todo_view.xml'],
+    'demo': [],
+}
