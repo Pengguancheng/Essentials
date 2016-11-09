7dd84552f195adf385f3d750dbedcea71778bb73
odoo <kimey583@gmail.com>
2016-11-08 18:20:23 -0800

    11.9  ch7 finish

---
+++ education/ch7_wizard/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': "ch7_wizard",
+    'version': '1.0',
+    'depends': ['ch3'],
+    'author': "alltop",
+    'website': "http://eoffice.alltop.com.tw",
+    'category': '',
+    'description': """
+    """,
+    'data': ['wizard_view.xml'],
+    'demo': [],
+}
